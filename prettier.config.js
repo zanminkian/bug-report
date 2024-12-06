288b841a4e1e3918645ddfba7ca2acf61781d73c
zanminkian <hellozmj@qq.com>
2024-12-06 17:30:08 +0000

    update

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,4 @@
-import { createRequire } from "node:module";
-
 export default {
-  plugins: [
-    createRequire(import.meta.url).resolve(
-      "@ianvs/prettier-plugin-sort-imports",
-    ),
-  ],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
   // importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
 };
